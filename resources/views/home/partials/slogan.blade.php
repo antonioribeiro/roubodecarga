d0aba102001967fce105f82e3e6c67dbe2a49244
Breno Trengrouse <breno@OGUM.local>
2017-08-31 03:06:23 -0300

    slogan

--- resources/views/home/partials/slogan.blade.php
+++ resources/views/home/partials/slogan.blade.php
@@ -1,7 +1,12 @@
 <section class="slogan">
-    <div class="row">
-        <div class="col-xs-12">
-            <p class="text-center">slogan</p>
-        </div>
-    </div>
+  <div class="container">
+      <div class="row">
+          <div class="main_slogan">
+              <div class="text-center">
+                  <h2>#nãocompreviolência</h2>
+                  <p class="lead">Mercadoria roubada. Quem compra faz vítimas.</p>
+              </div>
+          </div>
+      </div>
+  </div>
 </section>
