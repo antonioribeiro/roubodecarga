cb0514b992ad69a82053c4177e8c0429f12e1cd9
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2017-08-31 02:15:51 -0300

    Fix coding style

--- resources/views/home/partials/header.blade.php
+++ resources/views/home/partials/header.blade.php
@@ -3,13 +3,14 @@
         <div class="row">
             <div>
                 <div class="col-md-6">
-                  <p class="text-center">header</p>
+                    <p class="text-center">header</p>
                 </div>
+
                 <div class="col-md-6 text-right">
-                  <a href="#"><i class="fa fa-facebook"></i></a>
-                  <a href="#"><i class="fa fa-twitter"></i></a>
-                  <a href="#"><i class="fa fa-google-plus"></i></a>
-                  <a href="#"><i class="fa fa-linkedin"></i></a>
+                    <a href="#"><i class="fa fa-facebook"></i></a>
+                    <a href="#"><i class="fa fa-twitter"></i></a>
+                    <a href="#"><i class="fa fa-google-plus"></i></a>
+                    <a href="#"><i class="fa fa-linkedin"></i></a>
                 </div>
             </div>
         </div>
--- resources/views/home/partials/videos.blade.php
+++ resources/views/home/partials/videos.blade.php
@@ -5,63 +5,62 @@
         </div>
     </div>
 
-
     <div class="container">
-                <div class="row">
-                    <div class="main_service2">
-                        <div class="head_title text-center">
-                            <h2>Vídeos</h2>
-                            <p>condimentum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula.</p>
+        <div class="row">
+            <div class="main_service2">
+                <div class="head_title text-center">
+                    <h2>Vídeos</h2>
+                    <p>condimentum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula.</p>
+                </div>
+
+                <div class="service_content">
+                    <div class="col-md-6 col-sm-6">
+                        <div class="single_service2">
+                            <div class="single_service_left">
+                                <img src="assets/images/flaticon1.png" alt="">
+                            </div>
+                            <div class="single_service_right">
+
+                                <div class='embed-container'><iframe src='https://www.youtube.com/embed/EYm8dEWug1A' frameborder='0' allowfullscreen></iframe></div>
+
+                            </div>
                         </div>
+                    </div>
+                    <div class="col-md-6 col-sm-6">
+                        <div class="single_service2">
+                            <div class="single_service_left">
+                                <img src="assets/images/flaticon2.png" alt="">
+                            </div>
+                            <div class="single_service_right">
 
-                        <div class="service_content">
-                            <div class="col-md-6 col-sm-6">
-                                <div class="single_service2">
-                                    <div class="single_service_left">
-                                        <img src="assets/images/flaticon1.png" alt="">
-                                    </div>
-                                    <div class="single_service_right">
-                                        
-                                        <div class='embed-container'><iframe src='https://www.youtube.com/embed/EYm8dEWug1A' frameborder='0' allowfullscreen></iframe></div>
+                                <div class='embed-container'><iframe src='https://www.youtube.com/embed/EYm8dEWug1A' frameborder='0' allowfullscreen></iframe></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-sm-6">
+                        <div class="single_service2">
+                            <div class="single_service_left">
+                                <img src="assets/images/flaticon3.png" alt="">
+                            </div>
+                            <div class="single_service_right">
 
-                                    </div>
-                                </div>
+                                <div class='embed-container'><iframe src='https://www.youtube.com/embed/EYm8dEWug1A' frameborder='0' allowfullscreen></iframe></div>
                             </div>
-                            <div class="col-md-6 col-sm-6">
-                                <div class="single_service2">
-                                    <div class="single_service_left">
-                                        <img src="assets/images/flaticon2.png" alt="">
-                                    </div>
-                                    <div class="single_service_right">
-                                        
-                                        <div class='embed-container'><iframe src='https://www.youtube.com/embed/EYm8dEWug1A' frameborder='0' allowfullscreen></iframe></div>
-                                    </div>
-                                </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-sm-6">
+                        <div class="single_service2">
+                            <div class="single_service_left">
+                                <img src="assets/images/flaticon4.png" alt="">
                             </div>
-                            <div class="col-md-6 col-sm-6">
-                                <div class="single_service2">
-                                    <div class="single_service_left">
-                                        <img src="assets/images/flaticon3.png" alt="">
-                                    </div>
-                                    <div class="single_service_right">
-                                        
-                                        <div class='embed-container'><iframe src='https://www.youtube.com/embed/EYm8dEWug1A' frameborder='0' allowfullscreen></iframe></div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-md-6 col-sm-6">
-                                <div class="single_service2">
-                                    <div class="single_service_left">
-                                        <img src="assets/images/flaticon4.png" alt="">
-                                    </div>
-                                    <div class="single_service_right">
-                                        
-                                        <div class='embed-container'><iframe src='https://www.youtube.com/embed/EYm8dEWug1A' frameborder='0' allowfullscreen></iframe></div>
-                                    </div>
-                                </div>
+                            <div class="single_service_right">
+
+                                <div class='embed-container'><iframe src='https://www.youtube.com/embed/EYm8dEWug1A' frameborder='0' allowfullscreen></iframe></div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+        </div>
+    </div>
 </section>
