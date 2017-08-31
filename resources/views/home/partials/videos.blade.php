214a2d3aa8c2cca936489bb331942ac0f5d6c399
Breno Trengrouse <breno@OGUM.local>
2017-08-31 02:45:01 -0300

    videos- clean

--- resources/views/home/partials/videos.blade.php
+++ resources/views/home/partials/videos.blade.php
@@ -1,63 +1,24 @@
 <section class="videos">
-    <div class="row">
-        <div class="col-xs-12">
-            <p class="text-center">videos</p>
-        </div>
-    </div>
-
     <div class="container">
         <div class="row">
-            <div class="main_service2">
+            <div class="main_videos">
                 <div class="head_title text-center">
                     <h2>Vídeos</h2>
-                    <p>condimentum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula.</p>
+                    <p class="lead">condimentum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula.</p>
                 </div>
 
-                <div class="service_content">
-                    <div class="col-md-6 col-sm-6">
-                        <div class="single_service2">
-                            <div class="single_service_left">
-                                <img src="assets/images/flaticon1.png" alt="">
-                            </div>
-                            <div class="single_service_right">
-
-                                <div class='embed-container'><iframe src='https://www.youtube.com/embed/EYm8dEWug1A' frameborder='0' allowfullscreen></iframe></div>
-
-                            </div>
-                        </div>
+                <div class="videos_content">
+                    <div class="col-md-6 col-sm-6 video">
+                      <div class='embed-container'><iframe src='https://www.youtube.com/embed/EYm8dEWug1A' frameborder='0' allowfullscreen></iframe></div>
                     </div>
-                    <div class="col-md-6 col-sm-6">
-                        <div class="single_service2">
-                            <div class="single_service_left">
-                                <img src="assets/images/flaticon2.png" alt="">
-                            </div>
-                            <div class="single_service_right">
-
-                                <div class='embed-container'><iframe src='https://www.youtube.com/embed/EYm8dEWug1A' frameborder='0' allowfullscreen></iframe></div>
-                            </div>
-                        </div>
+                    <div class="col-md-6 col-sm-6 video">
+                      <div class='embed-container'><iframe src='https://www.youtube.com/embed/EYm8dEWug1A' frameborder='0' allowfullscreen></iframe></div>
                     </div>
-                    <div class="col-md-6 col-sm-6">
-                        <div class="single_service2">
-                            <div class="single_service_left">
-                                <img src="assets/images/flaticon3.png" alt="">
-                            </div>
-                            <div class="single_service_right">
-
-                                <div class='embed-container'><iframe src='https://www.youtube.com/embed/EYm8dEWug1A' frameborder='0' allowfullscreen></iframe></div>
-                            </div>
-                        </div>
+                    <div class="col-md-6 col-sm-6 video">
+                      <div class='embed-container'><iframe src='https://www.youtube.com/embed/EYm8dEWug1A' frameborder='0' allowfullscreen></iframe></div>
                     </div>
-                    <div class="col-md-6 col-sm-6">
-                        <div class="single_service2">
-                            <div class="single_service_left">
-                                <img src="assets/images/flaticon4.png" alt="">
-                            </div>
-                            <div class="single_service_right">
-
-                                <div class='embed-container'><iframe src='https://www.youtube.com/embed/EYm8dEWug1A' frameborder='0' allowfullscreen></iframe></div>
-                            </div>
-                        </div>
+                    <div class="col-md-6 col-sm-6 video">
+                      <div class='embed-container'><iframe src='https://www.youtube.com/embed/EYm8dEWug1A' frameborder='0' allowfullscreen></iframe></div>
                     </div>
                 </div>
             </div>
