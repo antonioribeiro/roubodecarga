5c6d69d3e0a3268ad06bdb6ebfeb6b293c074c92
Breno Trengrouse <breno@OGUM.local>
2017-08-30 22:58:38 -0300

    header - first mods

--- resources/views/home/partials/header.blade.php
+++ resources/views/home/partials/header.blade.php
@@ -1,7 +1,19 @@
-<section class="header">
-    <div class="row">
-        <div class="col-xs-12">
-            <p class="text-center">header</p>
-        </div>
-    </div>
-</section>
+        <section class="header">
+            <div class="container">
+                <div class="row">
+                    <div>
+                        <div class="col-md-6">
+                          <p class="text-center">header</p>
+                        </div>
+                        <div class="col-md-6">
+                          <div class="social-icon">
+                              <a href="#"><i class="fa fa-facebook"></i>teste</a>
+                              <a href="#"><i class="fa fa-twitter"></i>teste2</a>
+                              <a href="#"><i class="fa fa-google-plus"></i></a>
+                              <a href="#"><i class="fa fa-linkedin"></i></a>
+                          </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
